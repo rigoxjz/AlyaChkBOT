217a80a0eca8abffe8b818224b09b3d7bc84c7b2
rigoxjz <rigo2004j@gmail.com>
2025-03-08 14:27:19 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ if (empty($token)) {
 }
 include 'chk/functions.php';
 include 'chk/bot.php';
+include 'chk/chds/go.php';
 
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
